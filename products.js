1997a9738fe7d82f1489ae107e0024dccc76b0bf
Alexander Navarra <an942@live.mdx.ac.uk>
2021-11-17 01:50:22 +0000

    Adding other subjects from the shop

---
+++ products.js
@@ -0,0 +1,92 @@
+let products = [
+    {
+        "id": 1,
+        "image": "product.png",
+        "subject": "<strong>Math</strong>",
+        "location": "London",
+        "price": 100,
+        "availableInventory": 10,
+        "rating": 4
+    },
+    {
+        "id": 2,
+        "image": "product2.png",
+        "subject": "<strong>English</strong>",
+        "location": "London",
+        "price": 100,
+        "availableInventory": 10,
+        "rating": 4
+    },
+    {
+        "id": 3,
+        "image": "product.png",
+        "subject": "<strong>Science</strong>",
+        "location": "London",
+        "price": 100,
+        "availableInventory": 10,
+        "rating": 4
+    },
+    {
+        "id": 4,
+        "image": "product.png",
+        "subject": "<strong>History</strong>",
+        "location": "London",
+        "price": 100,
+        "availableInventory": 10,
+        "rating": 4
+    },
+    {
+        "id": 5,
+        "image": "product.png",
+        "subject": "<strong>Geography</strong>",
+        "location": "London",
+        "price": 100,
+        "availableInventory": 10,
+        "rating": 4
+    },
+    {
+        "id": 6,
+        "image": "product.png",
+        "subject": "<strong>Design and Technology</strong>",
+        "location": "London",
+        "price": 100,
+        "availableInventory": 10,
+        "rating": 4
+    },
+    {
+        "id": 7,
+        "image": "product.png",
+        "subject": "<strong>Art and Design</strong>",
+        "location": "London",
+        "price": 100,
+        "availableInventory": 10,
+        "rating": 4
+    },
+    {
+        "id": 8,
+        "image": "product.png",
+        "subject": "<strong>Music</strong>",
+        "location": "London",
+        "price": 100,
+        "availableInventory": 10,
+        "rating": 4
+    },
+    {
+        "id": 9,
+        "image": "product.png",
+        "subject": "<strong>Physical Education</strong>",
+        "location": "London",
+        "price": 100,
+        "availableInventory": 10,
+        "rating": 4
+    },
+    {
+        "id": 10,
+        "image": "product.png",
+        "subject": "<strong>Computing</strong>",
+        "location": "London",
+        "price": 100,
+        "availableInventory": 10,
+        "rating": 4
+    }
+] 
